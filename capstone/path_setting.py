efcd65c7131d34a53d5434da7729c1dfaed0245c
minjung0067 <minjung0067@naver.com>
2022-11-30 15:09:55 +0900

    feat: resize 경로 변경

--- capstone/path_setting.py
+++ capstone/path_setting.py
@@ -1,10 +1,12 @@
-# resizing_image - 이미지 폴더
+#RESIZE 
+#resizing_image - 이미지 폴더
 target_dir = r"C:/Users/jeongdaeyun/PycharmProjects/pythonProject/main/img" 
 
-after_resize_dir1 = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/resize/resize_dir1/"
-after_resize_dir2 = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/resize/resize_dir2/"
-after_resize_dir3 = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/resize/resize_dir3/"
-after_resize_dir4 = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/resize/resize_dir4/"
+#resize dir  
+save_dir1 = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/resize/resize_dir1/"
+save_dir2 = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/resize/resize_dir2/"
+save_dir3 = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/resize/resize_dir3/"
+save_dir4 = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/resize/resize_dir4/"
 
 #crawling save dir
 crawling_save_dir = r"/Users/mant/Desktop/project/school/capstone/pytorch_Are/capstone/naver_crawling_img"
